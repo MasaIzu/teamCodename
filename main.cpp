ebbeda64a2f732dfbcc2e71f4c4871463963e8d8
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-01-17 23:36:12 +0900

    もうしわけ

--- main.cpp
+++ main.cpp
@@ -79,8 +79,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		SCENE->PushDraw(MAP->map);
 
 
-		
-
 		//---------  ここまでにプログラムを記述  ---------//
 		// (ダブルバッファ)裏面
 		ScreenFlip();
